2a25d92362c17db3b49139fc513d029f6bc09b35
Jaimen Lathia <J_men@Jaimens-MacBook-Pro.local>
2018-05-26 16:12:15 +0100

    trying to upload package to Octopus

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -1,18 +1,17 @@
 
-project=$1
-versionNumber=$2
+Project=$1
+VersionNumber=1.0.$2
+PackageFile=${Project}.${VersionNumber}.zip
+
+# Install dependencies - move to docker image creation
 
 apt-get update
 apt-get install zip -y
 
 # Create Octopus Package
 
-zip ${project}.1.0.${versionNumber}.zip CloudFormation.yml
+zip ${PackageFile} CloudFormation.yml
 
-# Upload to Package Library
+# Upload to Octopus Package Library
 
-# curl -X POST -H "X-Octopus-ApiKey: $OctopusApiKey" \
-# -F "file=@\"${project}.1.0.0.zip\";filename=\"${YourApp}.1.0.0.zip\";type=application/zip" \
-# https://moonpig.octopus.com/api/packages/raw?replace=true
-
-# curl -X POST https://moonpig.octopus.com/api/packages/raw -H "X-Octopus-ApiKey: API-YOURAPIKEY" -F "data=@Demo.1.0.0.zip"
+curl -X POST https://moonpig.octopus.com/api/packages/raw -H "X-Octopus-ApiKey: ${OctopusApiKey}" -F "data=@${PackageFile}"
