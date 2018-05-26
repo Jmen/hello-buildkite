33a15523736714fa1a4acead03d4c7aa10c49421
Jaimen Lathia <J_men@Jaimens-MacBook-Pro.local>
2018-05-26 15:10:45 +0100

    lots of changes

---
+++ .scripts/build.sh
@@ -0,0 +1,11 @@
+
+project=$1
+versionNumber=$2
+
+zip ${project}.1.0.${versionNumber}.zip CloudFormation.yml
+
+# curl -X POST -H "X-Octopus-ApiKey: $OctopusApiKey" \
+# -F "file=@\"${project}.1.0.0.zip\";filename=\"${YourApp}.1.0.0.zip\";type=application/zip" \
+# https://moonpig.octopus.com/api/packages/raw?replace=true
+
+# curl -X POST https://moonpig.octopus.com/api/packages/raw -H "X-Octopus-ApiKey: API-YOURAPIKEY" -F "data=@Demo.1.0.0.zip"
