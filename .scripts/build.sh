566ec4e893cad5bdacff703fb91c9543e8c385f4
Jaimen Lathia <J_men@Jaimens-MacBook-Pro.local>
2018-05-27 00:09:07 +0100

    trying to create release

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -6,18 +6,20 @@ set -euo pipefail
 Project=$1
 VersionNumber=1.0.$2
 
-PackageFile=${Project}.${VersionNumber}.zip
+PackageFile=$Project.$VersionNumber.zip
 
 # Create Octopus Package
 
-zip ${PackageFile} CloudFormation.yml
+zip $PackageFile CloudFormation.yml
 
 # Upload to Octopus Package Library
 
-curl --request POST https://moonpig.octopus.com/api/packages/raw --header "X-Octopus-ApiKey: ${OctopusApiKey}" -F "data=@${PackageFile}" --fail
+curl --request POST https://moonpig.octopus.com/api/packages/raw --header "X-Octopus-ApiKey: $OctopusApiKey" -F "data=@$PackageFile"
 
 # Create a Release of the Project
 
-data=\'{ "Version" : "'"${VersionNumber}"'", "ProjectId" : "'"{$Project}"'", "ChannelId" : "Default" }\'
+ProjectId="$(curl https://moonpig.octopus.com/api/projects/$Project --header "X-Octopus-ApiKey: ${OctopusApiKey}" | jq -r '.Id')"
+ChannelId="$(curl https://moonpig.octopus.com/api/projects/$ProjectId/channels --header "X-Octopus-ApiKey: ${OctopusApiKey}" | jq -r '.Items[0].Id')"
 
-curl --request POST https://moonpig.octopus.com/api/releases --header "X-Octopus-ApiKey: ${OctopusApiKey}" --data ${data}
+curl --request POST https://moonpig.octopus.com/api/releases --header "X-Octopus-ApiKey: ${OctopusApiKey}" --data '{ "Version" : "'"$VersionNumber"'", "ProjectId" : "'"$ProjectId"'", "ChannelId" : "'"$ProjectId"'" }'
+
