3a29ffc57fb8504c5f4bc70ef85c131da0fd811d
Jaimen Lathia <J_men@Jaimens-MacBook-Pro.local>
2018-05-26 15:57:42 +0100

    adding dependency and creating zip

--- .scripts/build.sh
+++ .scripts/build.sh
@@ -2,7 +2,14 @@
 project=$1
 versionNumber=$2
 
-echo zip ${project}.1.0.${versionNumber}.zip CloudFormation.yml
+apt-get update
+apt-get install zip -y
+
+# Create Octopus Package
+
+zip ${project}.1.0.${versionNumber}.zip CloudFormation.yml
+
+# Upload to Package Library
 
 # curl -X POST -H "X-Octopus-ApiKey: $OctopusApiKey" \
 # -F "file=@\"${project}.1.0.0.zip\";filename=\"${YourApp}.1.0.0.zip\";type=application/zip" \
